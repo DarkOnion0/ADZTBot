c452d8dfd2c9afc5e81035dfd5d014de729462dd
DarkOnion0 <Darkgenius1@protonmail.com>
2020-12-24 10:13:55 +0100

    update add function in db.py

--- lib/db.py
+++ lib/db.py
@@ -1,6 +1,9 @@
 import sqlite3, os
 from datetime import datetime
 
+def timestamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 class user:
     """A class which handle all fonctions for adding user in the sqlite database"""
     def __init__(self, ConnectionPath):
@@ -13,6 +16,7 @@ class user:
         self.cursor.execute("CREATE TABLE IF NOT EXISTS Log (date INTEGER, logfile BLOB)")
         self.cursor.execute("CREATE TABLE IF NOT EXISTS UserData (id INTEGER PRIMARY KEY, username TEXT , creation_date TEXT, birthday TEXT, lvl INTEGER, lvl_xp INTEGER, os TEXT, description TEXT, game TEXT, coins FLOAT, awards_left, timestamp TEXT)")
         self.connection.commit()
+
 
     
     def add(self, username):
@@ -27,7 +31,6 @@ class user:
                 check = False
         
         if check == True: # if the user doesn't exist, create his profile
-            self.cursor.execute("INSERT INTO UserData(id, username, creation_date, timestamp) VALUES((SELECT max(id) FROM UserData)+1, ?, ?, ?)", (username, datetime.now().strftime("%Y-%m-%d %H:%M:%S"), datetime.now().strftime("%Y-%m-%d %H:%M:%S"),))
-
+            self.cursor.execute("INSERT INTO UserData(id, username, creation_date, timestamp) VALUES((SELECT max(id) FROM UserData)+1, ?, ?, ?)", (username, timestamp(), timestamp(),))
             print(username)
             self.connection.commit()
